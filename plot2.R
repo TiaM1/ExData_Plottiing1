036afa5e52e3507f944d21f6cdd6544ff988d2ad
TiaM1 <tiam@transanalytics.com>
2015-09-10 11:23:08 -0400

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -2,11 +2,11 @@ File <- "./household_power_consumption.txt"
 dataTable <- read.csv(File, header=T, sep=';', na.strings="?", 
             nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
 dataTable$Date <- as.Date(dataTable$Date, format="%d/%m/%Y")
-plt2data <- dataTable[dataTable$Date >= "2007-02-01" & Date <= "2007-02-02",]
+plot2data <- dataTable[dataTable$Date >= "2007-02-01" & Date <= "2007-02-02",]
 rm(dataTable)
 datetime <- paste(as.Date(plot2data$Date), plot2data$Time)
-datetime2 <- as.POSIXct(datetime)
-plot(plot2data$Global_active_power~datetime2, type="l",ylab="Global Active Power (kilowatts)", xlab="")
+plot2data$DateTime <- as.POSIXct(datetime)
+plot(plot2data$Global_active_power~DateTime, type="l",ylab="Global Active Power (kilowatts)", xlab="")
 dev.copy(png, file="plot2.png", height=480, width=480)
 png 
 5 
