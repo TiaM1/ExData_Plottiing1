84e031992468c54eace0d1ce5fd8ce0f06abe9a7
TiaM1 <tiam@transanalytics.com>
2015-09-10 11:30:55 -0400

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,13 +1,10 @@
 file <- "./household_power_consumption.txt"
 datatable <- read.csv(file, header=T, sep=';', na.strings="?",nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
-datatable_Date <- as.Date(datatable$Date, format="%d/%m/%Y")
-plot3data <- datatable[datatable$Date >= "2007-02-01" & datatable$Date <= "2007-02-02"]
+datatable$Date <- as.Date(datatable$Date, format="%d/%m/%Y")
+plot3data <- subset(datatable, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
 datetime <- paste(as.Date(plot3data$Date), plot3data$Time)
-plot3$DateTime <- as.POSIXct(datetime)
-with(plot3data, {plot(Sub_metering_1~DateTime, type="l", ylab="Global Active Power (kilowatts)", xlab="") lines(Sub_metering_2~plot3data$DateTime,col='Red')
-lines(Sub_metering_3~plot3$datetime,col='Blue')})
+plot3data$DateTime <- as.POSIXct(datetime)
+with(plot3data, { plot(Sub_metering_1~DateTime, type="l", ylab="Global Active Power (kilowatts)", xlab="")lines(Sub_metering_2~DateTime,col='Red')lines(Sub_metering_3~DateTime,col='Blue')})
 legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 dev.copy(png, file="plot3.png", height=480, width=480)
-png 
-5 
 dev.off()
